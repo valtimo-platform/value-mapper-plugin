1270cdc35e1bc0e51c019af0676e3f70ee423d7b
Anna Vissart <anna.vissart@ritense.com>
2025-12-18 12:21:15 +0100

    * update command unit test to accout for new functionality: ability to use defaultValue without transformations

--- backend/plugin/src/test/kotlin/com/ritense/valtimoplugins/valuemapper/domain/ValueMapperCommandTest.kt
+++ backend/plugin/src/test/kotlin/com/ritense/valtimoplugins/valuemapper/domain/ValueMapperCommandTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ritense.valtimoplugins.valuemapper.domain
 
 import com.fasterxml.jackson.databind.node.JsonNodeType
@@ -15,7 +31,6 @@ class ValueMapperCommandTest {
         assertDoesNotThrow {
             ValueMapperCommand(
                 defaultValue = "Works",
-                transformations = listOf(),
                 sourcePointer = "/this/is/a/source/pointer",
                 targetPointer = "/this/is/a/target",
             )
@@ -68,11 +83,12 @@ class ValueMapperCommandTest {
     }
 
     @Test
-    fun `should fail when defaultValue is defined but transformations not`() {
+    fun `should fail when skipCondition is defined for a CONVERT command`() {
         // then
         assertThrows<IllegalArgumentException> {
             ValueMapperCommand(
-                defaultValue = "Works",
+                skipCondition = "sourceValue == null",
+                operation = ValueMapperOperation.CONVERT,
                 sourcePointer = "/this/is/a/source/pointer",
                 targetPointer = "/this/is/a/target",
             )
